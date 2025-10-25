89a0e4cf1702829610363106b1e72cd018be92a2
missesenam <moneyesenam@gmail.com>
2025-10-25 18:41:51 +0000

    added vitest for testing and debugging

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: "jsdom", // 👈 this line makes Vitest simulate the browser DOM
+    setupFiles: "./src/setupTests.jsx",
+    pool: "threads", // helps on Windows
+  },
 });
