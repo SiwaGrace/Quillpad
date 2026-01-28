ca5f862de6891ce4e4d20ceb6a4c78f685dbf763
SiwaGrace <moneyesenam@gmail.com>
2026-01-28 05:12:27 +0000

    ignore modules

--- backend/index.js
+++ backend/index.js
@@ -26,8 +26,21 @@ const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
 //
+const allowedOrigins = [
+  "http://localhost:5173", // Local development
+  "https://quillpad-b0ce.onrender.com", // Your future Vercel URL
+];
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like (Postman, mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg =
+        "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
