18ce9b0f8be323ebc0081e05554b9fffbcfc2c03
SiwaGrace <moneyesenam@gmail.com>
2026-02-10 18:03:17 +0000

    i forgot to add 'const { sendWelcomeEmail } = require("brevoEmail"); to authcontroller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,7 @@ const asyncHandler = require("express-async-handler");
 require("dotenv").config();
 
 const crypto = require("crypto");
-const sendEmail = require("../utils/sendEmail");
+const { sendWelcomeEmail } = require("brevoEmail");
 
 const frontendUrl = process.env.FRONTEND_URL || "http://localhost:5173";
 
